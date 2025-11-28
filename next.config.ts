566b5fdc5f14060989a82977410a463fb4ce5151
Admin Pondok Tadzimussunnah <irfanmahdi.dev@gmail.com>
2025-11-29 05:57:06 +0700

    fix(config): add supabase hostname to next.config.ts images

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,16 @@ const nextConfig: NextConfig = {
   env: {
     ENCRYPTION_KEY: process.env.ENCRYPTION_KEY,
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lvlthftraeqqyveolzsm.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
